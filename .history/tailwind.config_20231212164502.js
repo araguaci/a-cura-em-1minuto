a36e413ee2092367d87f8c5d1385427b17be9cb7
araguaci <araguaci@gmail.com>
2025-09-15 20:59:47 -0300

    upgrade node22

--- .history/tailwind.config_20231212164502.js
+++ .history/tailwind.config_20231212164502.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './**/*.{html,js}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
